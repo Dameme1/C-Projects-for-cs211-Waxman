ea4badca40873095a63622bafd7a5de40fbfed5a
Dameme1 <109164898+Dameme1@users.noreply.github.com>
2022-10-23 19:30:13 -0400

    Update 8numberscross.cpp

--- 8numberscross.cpp
+++ 8numberscross.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std;
 
-bool ok(int q[], int c) // ok function, does all the tests to see if the number can be placed in a given box. 
+bool ok(int q[], int c) //  
 {
     static int a[8][5] = { // gives which boxes are adjacent to the box in question. (helper array)
         // Helper array
@@ -20,17 +20,17 @@ bool ok(int q[], int c) // ok function, does all the tests to see if the number 
     };
     for (int i = 0; i < c; i++) { // Makes sure there are no repeats. 
         if (q[i] == q[c])
-            return false; // returns false if there are repeats. 
+            return false; 
     }
     for (int i = 0; a[c][i] != -1; i++) { // makes sure there are no consecutive numbers in any adjacent box. 
         if (abs(q[c] - q[a[c][i]]) == 1) 
-            return false; // returns false if there a consecutive number in an adjacent box. 
+            return false; 
     }
     return true;
 }
 void print(int q[]) // print function to print any solution, 
 { // print function
-    static int sol_num = 0; // variable to count which solution number we are on currently. 
+    static int sol_num = 0; 
     cout << "Solution#: " << ++sol_num << endl;
     cout << " " << q[0] << q[1] << endl;
     cout << q[2] << q[3] << q[4] << q[5] << endl;
@@ -40,30 +40,30 @@ void print(int q[]) // print function to print any solution,
 
 int main()
 {
-    int q[8] = { 0 }; // declaration of the array that holds all the numbers. 
-    int c = 0; // declaration of column, which will be used in the tests. 
-    q[c] = 1; // putting 1 in the first box to initialize the problem 
+    int q[8] = { 0 }; //initializations
+    int c = 0; 
+    q[c] = 1;
 
-    while (c >= 0)  // outer while loop, makes sure that we aren't at -1, if we are then we will end the program because there are no more solutions. 
+    while (c >= 0)  // outer while loop, position check
     {
         c++;    // increment the column. 
-        if (c == 8) // if the column number is 8, we are off the board (boxes) and we have to print. 
+        if (c == 8) // position check 
         {
-            print(q); // calls the print function 
-            c--; // backtracks to find the next solution. 
+            print(q); 
+            c--; 
         } else
-        q[c] = 0; // sets the row equal to 0 so that when we start going through the 'rows' we can start off at one. 
-        while (c >= 0)  // inner while loop, makes sure that we aren't at -1, if we are then we will end the program becaues there are no more solutions. 
+        q[c] = 0; 
+        while (c >= 0)  // inner while loop, position check
         {
             q[c]++; // increment the 'row' 
-            if (q[c] > 8)  // if the 'row' number is greater than 8, that means that none of the numbers work and we have to back track to a previous column. 
+            if (q[c] > 8)  // position check
             {
-                c--; // backtrack by decrementing the column number.
-                } else if (ok(q, c)) { // calls the ok function and if it is true we will break out into the main function, or we will increment the row again. 
-            break; //breaks out into the outer while loop
+                c--; 
+                } else if (ok(q, c)) { 
+            break; 
             }
         } // inner loop
     } // outer loop
 
-    return 0; // ends the program when c == -1, meaning there are no more solutions. 
+    return 0; 
 } // main function 
