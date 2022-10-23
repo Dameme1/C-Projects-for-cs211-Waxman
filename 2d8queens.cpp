1c7a88ed52e32bf2c445d4a8c4225fa1383bd7f1
Dameme1 <109164898+Dameme1@users.noreply.github.com>
2022-10-23 18:12:42 -0400

    Update 2d8queens.cpp

--- 2d8queens.cpp
+++ 2d8queens.cpp
@@ -1,60 +1,57 @@
-// Tameem Ahmed 24177271
+// Tameem Ahmed
 // 2 Dimensional Array Program with Backtracking, GOTO
-// Question Index: 2 
 
 #include <iostream> 
 using namespace std;
 
 int main () {
-    static int sol_num = 1; // add counter for the solution number when printing out each solution
-    int q[8][8] = {0}; // create the chessboard and set all values equal to 0
-    int r = 0, c = 0; // initializing the values of the row and column as the integer 0 
-    q[r][c] = 1;//we start coding with placing 1st queen at [0][0]
+    static int sol_num = 1;       // initializations
+    int q[8][8] = {0}; 
+    int r = 0, c = 0;
+    q[r][c] = 1;
 
 nc:
-    c++; // increment into next column
-    if (c == 8) goto print; // if the column goes to 8, its off the chess board so we have to print the solution
-    r = -1; // row equal to -1, so when we go to the next row on line 20, it begins from index 0 after r++, the top of the column 
-
+    c++; //increment to next column
+    if (c == 8) goto print; // position check
+    r = -1; 
 nr:
     r++; // increments to next row
-    if (r == 8) goto backtrack; // if the row is 8 it means its off the chess board so we have to backtrack to a previous column 
+    if (r == 8) goto backtrack; // position check
     
-    for(int i = 0; i < c; i++){ //horizontal test by checking if a 1 (queen) is present horizontally
+    for(int i = 0; i < c; i++){ //horizontal test 
         if(q[r][i] == 1) goto nr;
     }
     
-    for(int i = 1;r - i >= 0 && c - i >= 0; i++){ // updiagonal test by testing if a 1 (queen) is present diagonally 
+    for(int i = 1;r - i >= 0 && c - i >= 0; i++){ // updiagonal test 
         if(q[r-i][c-i] == 1) goto nr;
     }
     
-    for(int i = 1; r + i < 8 && c - i >= 0; i++){ // downdiagonal test by testing if a 1 (queen) is present diagonally
+    for(int i = 1; r + i < 8 && c - i >= 0; i++){ // downdiagonal test 
         if(q[r+i][c-i] == 1) goto nr;
     }
 
     q[r][c] = 1; // sets a queen in that position because all the tests were passed. 
-    goto nc; // goes to the next column to continue the program 
-
+    goto nc; 
 backtrack:
-    c--; // goes to previous column to backtrack and change the queen into a different position 
-    if (c == -1) return 0; // if the column is -1, its off the board and there are no more possible solutions so end the program.
-    r = 0; // starts from index 0 so that we can check each row to find the queen so we can remove it
+    c--; // decrement column
+    if (c == -1) return 0; // position check
+    r = 0; 
     while(q[r][c] != 1){ // keeps iterating to the next row until the queen is found.
         r++;
     }
-    q[r][c]=0; // after the while loop, the queen was found so therefore we set q[r][c] to 0 so we remove the queen
-    goto nr; // we move on to the next row so we can find the next available spot.
+    q[r][c]=0; // set q[r][c] to 0  --- remove the queen
+    goto nr;
 print:
     cout << "Solution #: " << sol_num++ << endl; // prints out the solution number 
 
     for(int i = 0; i < 8; i++){ // nested for-loop to print out the chess board. 
         for (int j = 0; j < 8; j++){
-            cout << q[i][j] << " "; // prints out each individual row with spaces in between
+            cout << q[i][j] << " "; 
         }
-        cout << endl;//goes to the next line after each row is printed. 
+        cout << endl;
     }
 
-    goto backtrack;// goes to backtrack to find the next possible soltuion so we can find all the possible soltuions. not just one. 
+    goto backtrack; //finds the next solution
 
 
 
