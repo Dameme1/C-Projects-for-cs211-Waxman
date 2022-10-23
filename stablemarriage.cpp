1c808584a617db45649c0f7be9d7dd9d4c3fad1f
Dameme1 <109164898+Dameme1@users.noreply.github.com>
2022-10-23 19:43:53 -0400

    Update stablemarriage.cpp

--- stablemarriage.cpp
+++ stablemarriage.cpp
@@ -1,29 +1,35 @@
-// Tameem Ahmed 24177271
+// Tameem Ahmed
 // Stable Marriage, no GOTO's
-// Question Index: 5
+
 
 #include <iostream>
 using namespace std;
 
-bool ok(int q[], int c)
+bool ok(int q[], int c) // test function 
 {
-    static int mp[3][3] = { { 0, 2, 1 },
-                            { 0, 2, 1 }, 
-                            { 1, 2, 0 } };
+    //preferences are ranked from 0 through 2 where 0 is the best and 2 is the worst
+    
+    static int mp[3][3] = { { 0, 2, 1 },     // man 0's preferences
+                            { 0, 2, 1 },     // man 1's preferences
+                            { 1, 2, 0 } };   // man 2's preferences 
 
-    static int wp[3][3] = { { 2, 1, 0 }, 
-                            { 0, 1, 2 },
-                            { 2, 0, 1 } };
+    static int wp[3][3] = { { 2, 1, 0 },     // woman 0's preferences
+                            { 0, 1, 2 },     // woman 1's preferences
+                            { 2, 0, 1 } };   // woman 2's preferences
 
 
     for (int i = 0; i < c; i++){
-        if(q[c] == q[i] || (mp[i][q[c]] < mp[i][q[i]] && wp[q[c]][i] < wp[q[c]][c]) || ( mp[c][q[i]] < mp[c][q[c]] && wp[q[i]][c] < wp[q[i]][i]))
-        return false; 
+        if(q[c] == q[i] ||  //checks if woman 'c' is assigned to someone else already 
+           (mp[i][q[c]] < mp[i][q[i]] && wp[q[c]][i] < wp[q[c]][c]) || // checks if man 'i' likes man c's wife more than his own and vice versa (love reciprocated)
+           (mp[c][q[i]] < mp[c][q[c]] && wp[q[i]][c] < wp[q[i]][i]))   // checks if man 'c' likes man i's wife more than his own and vice versa (love reciprocated)
+        
+            return false; 
+        
     }
     return true;
 }
 
-void print(int q[]){
+void print(int q[]){  // prints given solution
     static int solution = 0;
     cout << "Solution #" << ++solution << ":\nMan\tWoman\n";
     for (int i = 0; i < 3; i++){
@@ -32,27 +38,25 @@ void print(int q[]){
 }
 
 int main (){
-     int q[3], c = 0; 
-    
-    
+     int q[3], c = 0; //initizalizations
     q[0] = 0; 
 
-        while (c >= 0) { 
-            c++; 
-            if (c == 3) { 
+        while (c >= 0) { // outer while loop, position check
+            c++; // increment column (man)
+            if (c == 3) { // position check
                 print(q);
                 c--; 
             } else
                 q[c] = -1; 
 
-            while (c >= 0) { 
-                q[c]++; 
+            while (c >= 0) { // inner while loop, position check
+                q[c]++; // increment row (woman)
                 if (q[c] == 3) 
                     c--; 
                 else if (ok(q, c)) 
                     break;
-            }
-        }
+            } // inner loop
+        } // outer loop
 
     return 0;
-}
+} // main function
