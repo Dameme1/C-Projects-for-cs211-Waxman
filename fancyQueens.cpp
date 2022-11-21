351a3aa92ac076d9c7ae2f1b3cc1153e2d3de6f8
Dameme1 <109164898+Dameme1@users.noreply.github.com>
2022-11-21 09:13:21 -0500

    Add files via upload

--- fancyQueens.cpp
+++ fancyQueens.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <stdio.h> 
 using namespace std;
 
 bool ok(int q[], int c) {
@@ -13,49 +12,41 @@ bool ok(int q[], int c) {
 void print(int q[]) {
    static int solution = 0;
    int i, j, k, l;
-   typedef char box[5][7];   // box is now a data type: a 5x7 2D array of characters
-   box bb, wb, *board[8][8]; // bb and wb are boxes (5x7 arrays).  board is an 8x8 array of pointers to boxes.
-                             // You don't have to clean up the board after printing, because it's not static,
-                             // so it'll be reinitialized the next time you call the funtion.
+   typedef char box[5][7];   // box is data type 
+   box bb, wb, *board[8][8]; // bb and wb are boxes.  board is an 8x8 array of pointers to boxes.
+                             
 
-   // Fill in bb (black box) and wb (white box).  They each represent a square of the chessboard.
-   // You only need to create one of each, since the chessboard can contain many pointers to the same box.
    for (i=0; i<5; i++)
-      for (j=0; j<7; j++) {
+      for (j=0; j<7; j++) {   // makes the 'wb' white and the 'bb' black. 
          wb[i][j] = ' ';
          bb[i][j] = char(219);
       }
    
-   // Create 2 more boxes to represent the queens, by drawing a picture of each queen in the 2D array.
-   static box bq = { {char(219),char(219),char(219),char(219),char(219),char(219),char(219)},
+   static box bq = { {char(219),char(219),char(219),char(219),char(219),char(219),char(219)},    //manually create the black queen template
                      {char(219),' '      ,char(219),' '      ,char(219),' '      ,char(219)},
                      {char(219),' '      ,' '      ,' '      ,' '      ,' '      ,char(219)},
                      {char(219),' '      ,' '      ,' '      ,' '      ,' '      ,char(219)},
                      {char(219),char(219),char(219),char(219),char(219),char(219),char(219)} };
                
-   static box wq = { {' '      ,' '      ,' '      ,' '      ,' '      ,' '      ,' '      },
+   static box wq = { {' '      ,' '      ,' '      ,' '      ,' '      ,' '      ,' '      },  //manually create the white queen template
                      {' '      ,char(219),' '      ,char(219),' '      ,char(219),' '      },
                      {' '      ,char(219),char(219),char(219),char(219),char(219),' '      },
                      {' '      ,char(219),char(219),char(219),char(219),char(219),' '      },
                      {' '      ,' '      ,' '      ,' '      ,' '      ,' '      ,' '      } };
 
-   // Fill board with pointers to bb and wb in alternate positions.
-   for(i=0; i<8; i++)
+   for(i=0; i<8; i++)    //creates alternating pattern of white and black boxes. 
       for(j=0; j<8; j++)
          if((i+j)%2 == 0)
             board[i][j] = &wb;
          else
             board[i][j] = &bb;
    
-   // Set up the current solution on the chessboard by placing pointers to bq and wq in the appropriate squares.
-   // i.e. connect solutions q[] with fancy queen bq/wq via pointer board
-   // Write the code.
     for(i = 0; i < 8; i++){
         for(j=0; j<8; j++){
-            if(((i+j)%2 == 0) && q[i] == j){
+            if(((i+j)%2 == 0) && q[j] == i){  //this will put a white queen on the black boxes
                 board[i][j] = &wq;
             }
-            if(((i+j)%2 != 0) && q[i] == j){
+            if(((i+j)%2 != 0) && q[j] == i){  //this will put a black queen on the white boxes
                 board[i][j] = &bq;
             }            
         }
@@ -63,27 +54,21 @@ void print(int q[]) {
 
    cout << "Solution #" << ++solution << ":\n ";
    
-   // Print upper border
-   for (i=0; i<7*8; i++)
+   for (i=0; i<7*8; i++)  //upper border
       cout << '_';
    cout << "\n";
 
-   // Print the board
    for (i=0; i<8; i++)          // for each board row
       for (k=0; k<5; k++) {     // for each box row
          cout << char(179);
          for (j=0; j<8; j++)    // for each board column
             for (l=0; l<7; l++) // for each box column
-               // board[i][j] is the box pointer in the ith row, jth column of the board.
-               // *board[i][j] is the box being pointed to.
-               // (*board[i][j])[k][l] is the kth row, lth column of the box.
-               cout << (*board[i][j])[k][l];
-         cout << char(179) << "\n";
-      }
+               cout << (*board[i][j])[k][l]; //[i][j] gives a pointer in that position, so *([i][j]) gives the 'box' that is being pointed to
+         cout << char(179) << "\n";                 // [k][l] is then used to iterate through each position in each 'box.' hence we print the board 
+      } 
 
-   // Print lower border
-   cout << " ";
-   for (i=0; i<7*8; i++)
+   cout << " ";   // lower border
+    for (i=0; i<7*8; i++)
       cout << char(196);
    cout << "\n\n";
 }
@@ -107,4 +92,4 @@ int main() {
         }
     }
     return 0;
-}
+} 
