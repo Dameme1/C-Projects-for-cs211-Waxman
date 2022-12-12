bdce7afdff627f5fba20c3c243ff548965fca862
Dameme1 <109164898+Dameme1@users.noreply.github.com>
2022-12-12 13:36:15 -0500

    Update robotPaths.cpp

--- robotPaths.cpp
+++ robotPaths.cpp
@@ -5,13 +5,13 @@
 using namespace std;
 
 int path(int i, int j){
-    if(i == 0 || j == 0) return 1; 
+    if(i == 0 || j == 0) return 1;  //this is the base case, there is only one path to the left most or the upper most row/column
     return path(i, j - 1) + path(i - 1, j);
 }
 
 int main(){
 
-    cout << path(4, 4) << endl;
+    cout << path(4, 4) << endl; //70
 
     return 0;    
 }
