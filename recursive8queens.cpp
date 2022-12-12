12033917b92011b206370c86556c8197467cf45c
Dameme1 <109164898+Dameme1@users.noreply.github.com>
2022-12-12 13:15:26 -0500

    Update recursive8queens.cpp

--- recursive8queens.cpp
+++ recursive8queens.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-bool ok(int q[], int c){
+bool ok(int q[], int c){.  //standard 'ok' function, does all the tests (diagonals and row checks)
     for(int i = 0; i < c; i++){
         if(q[c] == q[i] || abs(q[c] - q[i]) == (c-i)){
             return false;
@@ -12,7 +12,7 @@ bool ok(int q[], int c){
     return true;
 }
 
-void print(int q[]){
+void print(int q[]){ //Prints each solution 
     static int sol_num = 0;
     cout << "Solution #: " << ++sol_num << endl;
 
@@ -22,12 +22,13 @@ void print(int q[]){
             if(q[j] == i) cout << " X ";
             else cout << " O ";
         }
+        cout << endl;
     }
     cout << endl;
 }
 
-void next(int q[], int c){
-    if(c == 8){
+void next(int q[], int c){             //this is the recursive call
+    if(c == 8){                        
         print(q);
     }
     else{
